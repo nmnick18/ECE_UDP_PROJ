5a8cc7f1ae618471f030c94a1e911e66f19e6a87
zwartho <willstull17@gmail.com>
2020-11-29 17:29:48 -0500

    Fix spacing issues. Turn tabs into spaces.

--- client.cpp
+++ client.cpp
@@ -1,26 +1,24 @@
 /*
-
 	Client: Nikhil Malani & Will Stull 
 	Server: Raj Chaudhari & Segev Apter
 	CLI interface: Jeffrey Murray
 
+
 	Professor: Jeffrey Hurley
 	ECE-4122 Advanced Programming Techniques
 	Date: November 13, 2020
-
-
 */
 
-#include <stdio.h>	// input/output operations
-#include <stdlib.h>	// RNG, memory management, sorting, converting
-#include <sys/socket.h>	// Internet Protocol family
+#include <stdio.h>  // input/output operations
+#include <stdlib.h> // RNG, memory management, sorting, converting
+#include <sys/socket.h> // Internet Protocol family
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <cstring>	// c_str()
+#include <cstring>  // c_str()
 #include <iostream>
 
 
@@ -37,24 +35,25 @@ using std::string;
 
 string getFileName(const string& s) 
 {
-   char sep = '/';
+    char sep = '/';
 
-	#ifdef _WIN32
-		sep = '\\';
-	#endif
+    #ifdef _WIN32
+        sep = '\\';
+    #endif
 
-   size_t i = s.rfind(sep, s.length());
-   if (i != string::npos)
-      return(s.substr(i + 1, s.length() - i));
+    size_t i = s.rfind(sep, s.length());
+    
+    if (i != string::npos)
+        return(s.substr(i + 1, s.length() - i));
 
-   return("");
+    return("");
 }//end function
 
 
 /* Function allowing the creation of a socket
  * Returns a file descriptor
  */
-int UDPSocket (int port, char* ipaddr)
+int UDPSocket(int port, char* ipaddr)
 {
     int l;
 	int sockt;
@@ -62,96 +61,96 @@ int UDPSocket (int port, char* ipaddr)
 	sockt = socket(AF_INET,SOCK_DGRAM, 0);
 	
 	if (sockt < 0)
-	{
+    {
         std::cout << "socket fail" << std::endl;
         return EXIT_FAILURE;
-	}
-	
+    }
+
     //setting up destination socket address
-	l = sizeof(struct sockaddr_in);
-	bzero(&sock_serv, l);
-	sock_serv.sin_family = AF_INET;
-	sock_serv.sin_port = htons(port);
-	
+    l = sizeof(struct sockaddr_in);
+    bzero(&sock_serv, l);
+    sock_serv.sin_family = AF_INET;
+    sock_serv.sin_port = htons(port);
+
     if (inet_pton(AF_INET, ipaddr, &sock_serv.sin_addr) == 0)
-	{
-		std::cout << "Invalid IP adress" << std::endl;
-		return EXIT_FAILURE;
-	}
+    {
+        std::cout << "Invalid IP adress" << std::endl;
+        return EXIT_FAILURE;
+    }
 	
     return sockt;
 }//end function
 
 
 
-int main (int argc, char**argv)
+int main(int argc, char**argv)
 {
     int sockt,fd;
     char buf[BUF_SIZE];
     long count = 0, m, sz;
-	long int n;
+    long int n;
     int l = sizeof(struct sockaddr_in);
-	struct stat buffer;
+    struct stat buffer;
     
-	if (argc != 4)
-	{
-		std::cout << "Error usage : " << argv[0] << " <ip_server> <port_server> <file directory>" << std::endl;
-		return EXIT_FAILURE;
-	}
+    if (argc != 4)
+    {
+        std::cout << "Error usage : " << argv[0] << " <ip_server> <port_server> <file directory>" << std::endl;
+        return EXIT_FAILURE;
+    }
     
     sockt=UDPSocket(atoi(argv[2]), argv[1]);
     
     std::string path = argv[3];
 
-	if ((fd = open(argv[3],O_RDONLY)) == -1)
-	{
-		std::cout<<"open fail"<<std::endl;
-		return EXIT_FAILURE;
-	}
+    if ((fd = open(argv[3],O_RDONLY)) == -1)
+    {
+        std::cout<<"open fail"<<std::endl;
+        return EXIT_FAILURE;
+    }
     
-	if (stat(argv[3],&buffer) == -1)
-	{
-		std::cout << "stat fail" << std::endl;
-		return EXIT_FAILURE;
-	}
-	else
-		sz = buffer.st_size;
-	
-	// send file name to server
-	m = sendto(sockt, getFileName(path).c_str(), 256, 0, (struct sockaddr*)&sock_serv, l);
-	
-	bzero(&buf,BUF_SIZE);	// clear buffer
+    if (stat(argv[3],&buffer) == -1)
+    {
+        std::cout << "stat fail" << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+        sz = buffer.st_size;
 
-    n=read(fd,buf,BUF_SIZE);	// initial read of file
+    // send file name to server
+    m = sendto(sockt, getFileName(path).c_str(), 256, 0, (struct sockaddr*)&sock_serv, l);
 	
-	// read and send bytes of file
-	while(n)
-	{
-		if(n < 0)
-		{
-			std::cout<<"read fails"<<std::endl; 
-			return EXIT_FAILURE;
-		}
+    bzero(&buf,BUF_SIZE);   // clear buffer
+
+    n=read(fd,buf,BUF_SIZE);    // initial read of file
+	
+    // read and send bytes of file
+    while (n)
+    {
+        if (n < 0)
+        {
+            std::cout<<"read fails"<<std::endl; 
+            return EXIT_FAILURE;
+        }
 		
-		m = sendto(sockt, buf, n, 0, (struct sockaddr*)&sock_serv, l);
+        m = sendto(sockt, buf, n, 0, (struct sockaddr*)&sock_serv, l);
 		
-		if(m < 0)
-		{
-			std::cout << "send error" << std::endl;
-			return EXIT_FAILURE;
-		}
+        if (m < 0)
+        {
+            std::cout << "send error" << std::endl;
+            return EXIT_FAILURE;
+        }
 		
-		count += m;
-		bzero(buf, BUF_SIZE);
+        count += m;
+        bzero(buf, BUF_SIZE);
         n = read(fd, buf, BUF_SIZE);
-	}
+    }
 
-	m = sendto(sockt, buf, 0, 0, (struct sockaddr*)&sock_serv, l);	// send remaining bytes
-    
-	std::cout << "The file name is: " << getFileName(path) << std::endl;
-	std::cout << "Bytes transferred: " << count << std::endl;
+    m = sendto(sockt, buf, 0, 0, (struct sockaddr*)&sock_serv, l);  // send remaining bytes
 
-	close(sockt);
-	close(fd);
-	return EXIT_SUCCESS;
+    std::cout << "The file name is: " << getFileName(path) << std::endl;
+    std::cout << "Bytes transferred: " << count << std::endl;
+
+    close(sockt);
+    close(fd);
+    return EXIT_SUCCESS;
 }
--- server.cpp
+++ server.cpp
@@ -9,16 +9,16 @@
 	Date: November 13, 2020
 */
 
-#include <stdio.h>	// input/output operations
-#include <stdlib.h>	// RNG, memory management, sorting, converting
-#include <sys/socket.h>	// for non-windows system
-#include <arpa/inet.h>	//Internet Protocol family
+#include <stdio.h>  // input/output operations
+#include <stdlib.h> // RNG, memory management, sorting, converting
+#include <sys/socket.h> // for non-windows system
+#include <arpa/inet.h>  //Internet Protocol family
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <cstring>	// c_str()
+#include <cstring>  // c_str()
 #include <string>
 #include <iostream>
 
@@ -36,96 +36,92 @@ struct sockaddr_in sock_serv,clt;
  */
 int UDPSocket(int usPort)
 {
-	int sockt;
+    int sockt;
 
-	//creating a socket sockt
-	sockt = socket(AF_INET,SOCK_DGRAM, 0);
+    //creating a socket sockt
+    sockt = socket(AF_INET,SOCK_DGRAM, 0);
 
-	if (sockt < 0)
-	{
+    if (sockt < 0)
+    {
         std::cout << "ERROR opening socket" << std::endl; 
         return 1;
-	}
+    }
 
-	//zero out the server address variable
+    //zero out the server address variable
     //bzero(&sock_serv,sizeof(struct sockaddr_in));
 	
-	//zero out the server address variable
+    //zero out the server address variable
     memset(&sock_serv, 0, sizeof(struct sockaddr_in));
 	
-	//initialize the server address
-	sock_serv.sin_family = AF_INET;
+    //initialize the server address
+    sock_serv.sin_family = AF_INET;
 	
-	//convert portnumber from host to network
-	sock_serv.sin_port = htons(usPort);
+    //convert portnumber from host to network
+    sock_serv.sin_port = htons(usPort);
 
-	sock_serv.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_serv.sin_addr.s_addr = htonl(INADDR_ANY);
 
-
-	//bind the socket to the portnumber
-	if(bind(sockt, (struct sockaddr *)&sock_serv, sizeof(struct sockaddr_in)) < 0)
-	{
-		std::cout << "ERROR on binding" << std::endl;
-		return 1;
-	}
+    //bind the socket to the portnumber
+    if(bind(sockt, (struct sockaddr *)&sock_serv, sizeof(struct sockaddr_in)) < 0)
+    {
+        std::cout << "ERROR on binding" << std::endl;
+        return 1;
+    }
     
     return sockt;
-
 }//end Create_server_socket function
 
 
 
 int main (int argc, char** argv)
 {
-	int file; 
-	int sockt;
-	char buf[BUF_SIZE];
-	off_t count = 0, n; // long type
-	char filename[256];
+    int file; 
+    int sockt;
+    char buf[BUF_SIZE];
+    off_t count = 0, n; // long type
+    char filename[256];
     unsigned int l = sizeof(struct sockaddr_in);
     
-	if (argc != 2)
-	{
-		std::cout << "Error usage : " << argv[0] << " " << "<port_serv>" << std::endl;
-		return 1;
-	}
+    if (argc != 2)
+    {
+        std::cout << "Error usage : " << argv[0] << " " << "<port_serv>" << std::endl;
+        return 1;
+    }
     
     sockt = UDPSocket(atoi(argv[1]));	// set up socket file descriptor
+
+    bzero(filename, 256);	// clear filename
+    n = recvfrom(sockt, &filename, 256, 0, (struct sockaddr *)&clt, &l);	// receive filename
+
+    std::cout << "Creating the output file : " << filename << std::endl;
+
+    //file opening
+    if((file = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600)) < 0)
+    {
+        std::cout << "fail to open the file" << std::endl;
+        return 1;
+    }
     
-
-	bzero(filename, 256);	// clear filename
-	n = recvfrom(sockt, &filename, 256, 0, (struct sockaddr *)&clt, &l);	// receive filename
-
-	std::cout << "Creating the output file : " << filename << std::endl;
-
-	//file opening
-	if((file = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600)) < 0)
-	{
-		std::cout << "fail to open the file" << std::endl;
-		return 1;
-	}
-    
-	// preparation of the shipment
-	bzero(&buf, BUF_SIZE);
+    // preparation of the shipment
+    bzero(&buf, BUF_SIZE);
 	
-    n=recvfrom(sockt, &buf, BUF_SIZE, 0, (struct sockaddr *)&clt, &l);	// receive first # of bytes
+    n = recvfrom(sockt, &buf, BUF_SIZE, 0, (struct sockaddr *)&clt, &l);	// receive first # of bytes
 
-	// receive remaining bytes
-	while(n)
-	{
-		std::cout << n << " of data received" << std::endl; 
+    // receive remaining bytes
+    while(n)
+    {
+        std::cout << n << " of data received" << std::endl; 
 
-		if (n < 0)
-		{
-			std::cout << "Unable to read file "<< std::endl;
-			return 1;
-		}
+        if (n < 0)
+        {
+            std::cout << "Unable to read file "<< std::endl;
+            return 1;
+        }
 
-		count += n;
-		write(file, buf, n);
-		bzero(buf, BUF_SIZE);
+        count += n;
+        write(file, buf, n);
+        bzero(buf, BUF_SIZE);
         n = recvfrom(sockt, &buf, BUF_SIZE, 0, (struct sockaddr *)&clt, &l);
-	}
+    }
 
 }
-    
